00deccf33efcb7e3f6f66f19bb7b7c91513413c1
Maneer Ali <maneerali375@gmail.com>
2025-03-22 00:49:42 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello this is just a tutorial for CI/CD Pipeline"
+    return "Test app for CI/CD Pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
