280a6e15793cf74148e3fb5bfa6b8e5ccd55a6ef
Maneer Ali <maneerali375@gmail.com>
2025-03-22 00:28:13 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Hello, this is a CI/CD demo with Cloud Build and Cloud Run! and my name is maneer ali and see the magic"
+    return "Hello this is just a tutorialfor CI/CD Pipeline"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
